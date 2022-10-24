99e63009628f63de7003ee3b488704f1c74608de
AlinaBlo <82020749+AlinaBlo@users.noreply.github.com>
2022-10-24 13:35:37 +0300

    Add files via upload

---
+++ moviesUtils.js
@@ -0,0 +1,21 @@
+﻿prefix = "https://images-na.ssl-images-amazon.com/images/M";
+function init() {
+    posterArr = [];
+    for (var i = 0; i < posters.length; i++) {
+        posterArr[posters[i].imdbId] = posters[i].Poster;
+    }
+    handleData(movies, posterArr);
+}
+
+function handleData(movies, posterArr) {
+    str = "";
+    for (var i = 0; i < movies.length; i++) {
+        if (movies[i].imdb_title_id[2] == 0) {
+            var id = movies[i].imdb_title_id.substring(3, movies[i].imdb_title_id.length);
+        }
+        else {
+            var id = movies[i].imdb_title_id.substring(2, movies[i].imdb_title_id.length);
+        }
+        movies[i].poster_path = prefix + posterArr[id];
+    }
+}
